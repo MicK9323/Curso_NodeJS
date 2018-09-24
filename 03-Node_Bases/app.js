8eacf6a421fdfa64278c4e234b6c06b1a99d9882
Miguel Cortegana <miguel.cortegana93@gmail.com>
2018-09-24 01:10:59 -0500

    yargs

--- 03-Node_Bases/app.js
+++ 03-Node_Bases/app.js
@@ -1,8 +1,43 @@
-const argv = require('yargs').argv;
-const {generar} = require('./utils/operaciones');
+const {generar, listar} = require('./utils/operaciones');
+const argv = require('yargs')
+    .command('listar', 
+        'Imprimir en consola la tabla de multiplicar de un número enviado como parámetro', {
+            base: {
+                demand: true,
+                alias: 'b'
+            },
+            limite: {
+                alias: 'l',
+                default: 12
+            }
+        })
+    .command('crear',
+        'Crea un archivo de texto plano con una tabla de impresión', {
+            base: {
+                alias: 'b',
+                demand: true
+            },
+            limite: {
+                alias: 'l',
+                default: 12
+            }
+        })
+    .help()
+    .argv;
 
-let base = process.argv[2].split('=')[1];
+let comando = argv._[0];
 
-generar(base)
-    .then(result => console.log(result))
-    .catch(err => console.log(err));
+switch (comando) {
+    case 'crear':
+        generar(argv.limite, argv.base)
+            .then(result => console.log(result))
+            .catch(err => console.log(err));
+        break;
+    case 'listar':
+        listar(argv.limite, argv.base)
+            .then(result => console.log(result))
+            .catch(err => console.log(err));
+        break;
+    default:
+        console.log(`No se reconoce una instrucción para el comando '${comando}'`);
+}
--- 03-Node_Bases/utils/operaciones.js
+++ 03-Node_Bases/utils/operaciones.js
@@ -1,6 +1,6 @@
 const {generarArchivo} = require('./files');
 
-let multiplicar = async (base) => {
+let multiplicar = async (limite, base) => {
     if (!Number(base)) {
        throw new Error(`La base ${base} introducida no es un número válido`);
     }
@@ -9,11 +9,23 @@ let multiplicar = async (base) => {
     contenido += `Tabla del ${base}\n`;
     contenido += `----------------------\n\n`;
 
-    for(let i=1; i<=12;i++){
+    for(let i=1; i<=limite;i++){
         contenido += `${base} x ${i} = ${base*i}\n`;
     }
 
     return {base, contenido};
+};
+
+let listar = async (limite, base) => {
+    if (!Number(base)) {
+        throw new Error(`La base ${base} introducida no es un número válido`);
+    }
+    if (!Number(limite)) {
+        throw new Error(`El limite ${limite} introducido no es un número válido`);
+    }
+
+    let data = await multiplicar(limite, base);
+    return data.contenido;
 };
 
 let generar = async (base) => {
@@ -21,4 +33,4 @@ let generar = async (base) => {
     return await generarArchivo(data);
 };
 
-module.exports = {generar};
+module.exports = {generar, listar};
